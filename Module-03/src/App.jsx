1258b0ea887bcce4a3913009ade387f9905459e8
minhajul <dev.minhajul@gmail.com>
2025-01-29 22:24:30 +0600

    check tailwindcss upgrade check

--- Module-03/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import PreservingChallange1 from "./components/PreservingChallange1";
-
-export default function App() {
-  return (
-    <div>
-      <PreservingChallange1 />
-    </div>
-  );
-}
---
+++ Module-03/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="text-4xl font-bold text-center">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident
+      voluptates, sint porro accusantium fugiat maiores optio deserunt
+      dignissimos, rem tempora, omnis modi ad minima aliquid nobis assumenda
+      consequatur odit aperiam?
+    </div>
+  );
+}
+
+export default App;
